ef2b9b1b7a56c683ff3b3c05e4170ee25fc1a4b7
FMABr <felipemuamorim@gmail.com>
2024-11-16 11:51:09 -0400

    flutter project

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:chat/models/app_service.dart';
+import 'package:chat/pages/current_page.dart';
+import 'package:chat/widgets/add_icon.dart';
+import 'package:chat/widgets/bottom_bar.dart';
+import 'package:chat/widgets/eager_initialization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// This file is the entry point of the Bonsoir example project.
+/// The full source code is available here :
+/// https://github.com/Skyost/Bonsoir/tree/master/packages/bonsoir/example.
+///
+/// Feel free to check the available code snippets as well :
+/// https://bonsoir.skyost.eu/docs#getting-started.
+
+/// Plugin's main method.
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DefaultAppService.initialize();
+  runApp(const ProviderScope(child: BonsoirExampleMainWidget()));
+}
+
+/// The main widget.
+class BonsoirExampleMainWidget extends StatelessWidget {
+  /// Creates a new main widget instance.
+  const BonsoirExampleMainWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) => EagerInitialization(
+        child: MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Private Chat'),
+              actions: const [AddIcon()],
+              centerTitle: false,
+            ),
+            body: const CurrentPageWidget(),
+            bottomNavigationBar: const BottomBar(),
+          ),
+        ),
+      );
+}
---
+++ lib/widgets/add_icon.dart
@@ -0,0 +1,43 @@
+import 'package:bonsoir/bonsoir.dart';
+import 'package:chat/dialogs/broadcast_prompt.dart';
+import 'package:chat/dialogs/discovery_prompt.dart';
+import 'package:chat/models/broadcast.dart';
+import 'package:chat/models/discovery.dart';
+import 'package:chat/pages/pages.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// Adds a broadcast or a discovery.
+class AddIcon extends ConsumerWidget {
+  /// Creates a new "Add" icon instance.
+  const AddIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    AppPage currentPage = ref.watch(appPageProvider);
+    return IconButton(
+      onPressed: () async {
+        switch (currentPage) {
+          case AppPage.discoveries:
+            String? type = await DiscoveryPromptDialog.prompt(context);
+            if (type != null) {
+              ref.read(discoveryModelProvider).start(type);
+            }
+            break;
+          case AppPage.broadcasts:
+            BonsoirService? service =
+                await BroadcastPromptDialog.prompt(context);
+            if (service != null) {
+              ref.read(broadcastModelProvider).start(service);
+            }
+            break;
+        }
+      },
+      icon: const Icon(Icons.add),
+      tooltip:
+          'Add a ${currentPage == AppPage.discoveries ? 'discovery' : 'broadcast'}',
+    );
+  }
+}
