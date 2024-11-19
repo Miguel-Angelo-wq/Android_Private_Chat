1bbbec7dfdb9aa0bf404438e8b2e4bbebcc5db26
FMABr <felipemuamorim@gmail.com>
2024-11-19 15:02:08 -0400

    progress

--- app/index.tsx
+++ app/index.tsx
@@ -1,81 +1,56 @@
 import {
-  fromServiceBrowserEvent,
+  MessagingClient,
+  MessagingClientServiceSearchResult,
+  MessagingClientServiceSearchUpdate,
   MessagingServer,
-  ServiceBrowser,
-  ServiceBrowserEventName,
   ServiceBrowserServiceFoundNativeEvent,
   ServiceBrowserServiceLostNativeEvent,
 } from "@figuredev/react-native-local-server";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { AppRegistry, Text } from "react-native";
-import {
-  MD3LightTheme,
-  PaperProvider,
-  SegmentedButtons,
-  Surface,
-} from "react-native-paper";
+import DeviceInfo from "react-native-device-info";
+import { MD3LightTheme, PaperProvider, Surface } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { expo } from "../app.json";
-import Zeroconf from "react-native-zeroconf";
-import DeviceInfo from "react-native-device-info";
+import _ from "lodash";
 
 export default function Index() {
-  const server = new MessagingServer(DeviceInfo.getDeviceId());
-  // const service = useRef<Zeroconf>(new Zeroconf());
-  // const browser = useRef<ServiceBrowser>(
-  //   new ServiceBrowser(DeviceInfo.getDeviceId())
-  // );
-
-  useEffect(() => {
-    // service.current.publishService(
-    //   "private-chat",
-    //   "tcp",
-    //   "local.",
-    //   "sla que porra",
-    //   4000
-    // );
-    // browser.current.start({ type: "_private-chat._tcp" });
-
-    server.start(
-      {
-        service: { name: "_private-chat._tcp", id: DeviceInfo.getDeviceId() },
-        name: DeviceInfo.getBaseOsSync() + DeviceInfo.getDeviceNameSync(),
-        port: 4000,
-        discovery: {
-          group: "group",
-          name: "name",
-        },
-      },
-      (message) => message,
-      undefined
-    );
-  }, []);
-
-  // fromServiceBrowserEvent(
-  //   "Felipe",
-  //   ServiceBrowserEventName.ServiceFound
-  // ).subscribe(addService);
-
-  // fromServiceBrowserEvent(
-  //   "Felipe",
-  //   ServiceBrowserEventName.ServiceLost
-  // ).subscribe(removeService);
-
   const [serviceList, setServiceList] = useState<
-    ServiceBrowserServiceFoundNativeEvent[]
+    MessagingClientServiceSearchResult[]
   >([]);
 
-  function addService(event: ServiceBrowserServiceFoundNativeEvent) {
-    const newList = serviceList.concat([event]);
-    setServiceList(newList);
-  }
+  const server = new MessagingServer(DeviceInfo.getDeviceId());
+  const client = new MessagingClient(DeviceInfo.getDeviceId(), "private-chat");
+  useEffect(() => {
+    client.startServiceSearch().subscribe();
 
-  function removeService(event: ServiceBrowserServiceLostNativeEvent) {
-    const index = serviceList.findIndex(
-      (service) => service.host === event.host
-    );
-    setServiceList(serviceList.toSpliced(index, 1));
-  }
+    server
+      .start(
+        {
+          service: { name: "private-chat", id: DeviceInfo.getDeviceId() },
+          name: DeviceInfo.getBaseOsSync() + DeviceInfo.getDeviceNameSync(),
+          port: 4000,
+          discovery: {
+            group: "private-chat",
+            name: DeviceInfo.getBaseOsSync() + DeviceInfo.getDeviceNameSync(),
+          },
+        },
+        (message) => message,
+        undefined
+      )
+      .subscribe();
+  }, []);
+
+  client.getSearchUpdate$().subscribe(({ update }) => {
+    const newList =
+      update.type === MessagingClientServiceSearchUpdate.ServiceFound
+        ? serviceList.concat([update.service])
+        : update.type === MessagingClientServiceSearchUpdate.ServiceLost
+        ? _.differenceWith(serviceList, [update.service], _.isEqual)
+        : [];
+
+    setServiceList(newList);
+  });
 
   return (
     <PaperProvider theme={MD3LightTheme}>
